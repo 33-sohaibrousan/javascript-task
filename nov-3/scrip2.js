afe10459686e3e9d0297d1a9b722af345ae64ad4
unknown <33-sohaibrousan>
2022-11-03 16:06:39 +0300

    script

---
+++ nov-3/scrip2.js
@@ -0,0 +1,4 @@
+const pi=3.14;
+var r=5;
+var circleArea=pi*r*r;
+document.write(circleArea);
---
+++ nov-3/script.js
@@ -0,0 +1,23 @@
+var mark = -1;
+if (mark < 0){
+    document.write("hehehehe");
+} 
+ else if (mark > 0 & mark <=49){
+    document.write("fail");
+}
+else if (mark >= 50 & mark <=59){
+    document.write("grade : D");
+}
+else if(mark >=60 & mark <=69){
+    document.write("grade : c");
+}
+else if (mark >= 70 & mark <=79){
+    document.write("grade : b");
+}
+else if (mark >=80 & mark <=100){
+   document.write("grade : A");
+    
+}
+else if ( mark > 100){
+    document.write("you are not a smart");
+}
