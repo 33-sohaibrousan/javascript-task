2b4848994ba6f3f22a6f69b9488d39bb9cb62b44
unknown <33-sohaibrousan>
2022-11-06 17:10:53 +0300

    js

---
+++ nov-6/script.js
@@ -0,0 +1,52 @@
+var month = Number(prompt("inter your birthday"))
+switch (month){
+case 1 :
+   
+    document.write("january");
+    break;
+    case 2 :
+        document.write("fabrewary");
+
+    break;
+    case 3 :
+        document.write("march");
+
+    break;
+    case 4 :
+        document.write("april");
+
+    break;
+    case 5 :
+        document.write("may");
+
+    break;
+    case 6 :
+        document.write("june");
+
+    break;
+    case 7 :
+        document.write("july");
+
+    break;
+    case 8 :
+        document.write("august");
+
+    break;
+    case 9 :
+        document.write("septemper");
+
+    break;
+    case 10 :
+        document.write("octoper");
+
+    break;
+    case 11 :
+        document.write("novemper");
+
+    break;
+    case 12 :
+        document.write("desemper");
+
+    break;
+  
+}
---
+++ nov-6/script4.js
@@ -0,0 +1,11 @@
+let i = 1 ;
+let sum = 0;
+while(i <=100){
+    sum+=i;
+    if(sum >=100)
+    {
+        break;
+    }
+document.write(sum ,"<br>");
+i++
+}
